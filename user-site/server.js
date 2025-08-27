e14dbbdcd7c7c51f3e38df0711a6235f951e131f
Zi Ning <tanzining0109@gmail.com>
2025-08-27 22:10:54 +0800

    Update code.

--- user-site/server.js
+++ user-site/server.js
@@ -12,7 +12,6 @@ app.set('views', path.join(__dirname, 'views'));
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true })); 
-app.use(express.json());
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
